484048e2c23b4a7472ccacc518ac511c49ed5e5f
EDEN <EDBEK@YAHOO.COM>
2020-10-26 16:29:10 -0500

    Integrated Firebase to app. Still need to handle errors for adding documents to firestore database

--- src/components/UploadBeat.jsx
+++ src/components/UploadBeat.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { db } from "../firebase";
+import "firebase/firestore";
 
 function UploadBeat() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+
+  // const beatName = document.getElementById("beat-name");
+  // const beatUpload = document.getElementById("beat-upload");
+
+  db.collection("Beats")
+    .doc("dMSSRUndR5ZgKnutosrm")
+    .set({
+      name: "A beat",
+      beatUpload: "goats.mp3",
+    })
+    .then(function (docRef) {
+      console.log("Document written ");
+    })
+    .catch(function (error) {
+      console.log("Error adding document: ", error);
+    });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="beatName"> Name: </label>
-      <input name="beatName" ref={register} />
+      <input name="beatName" id="beat-name" ref={register} />
       <br />
       <label htmlFor="beatUpload"> Upload your beat: </label>
       <br />
-      <input type="file" name="beatUpload" />
+      <input type="file" name="beatUpload" id="beat-upload" />
       <button>Submit</button>
     </form>
   );
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// NOTE: import only the Firebase modules that you need in your app... except
+// for the second line, which makes both the linter and react-firebase happy
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// Initalize Firebase.
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4qNOEczkKO1bdj3LvvWLyvzd0RxD-2oA",
+  authDomain: "producer-beats.firebaseapp.com",
+  databaseURL: "https://producer-beats.firebaseio.com",
+  projectId: "producer-beats",
+  storageBucket: "producer-beats.appspot.com",
+  messagingSenderId: "983585076088",
+  appId: "1:983585076088:web:94ea1e4b3d228184cb0e8d",
+};
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+export const fb = firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
