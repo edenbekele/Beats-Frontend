953d269436ef94b970537ef73778032c594869bd
EDEN <EDBEK@YAHOO.COM>
2020-10-30 15:39:05 -0500

    Connected Firestore database to app, ability to add dummy data from app

--- src/components/UploadBeat.jsx
+++ src/components/UploadBeat.jsx
@@ -5,34 +5,40 @@ import "firebase/firestore";
 
 function UploadBeat() {
   const { register, handleSubmit } = useForm();
+  // const db = firebase.firestore();
 
-  // const beatName = document.getElementById("beat-name");
-  // const beatUpload = document.getElementById("beat-upload");
+  const beatName = document.getElementById("beat-name");
+  const beatUpload = document.getElementById("beat-upload");
 
-  db.collection("Beats")
-    .doc("dMSSRUndR5ZgKnutosrm")
-    .set({
-      name: "A beat",
-      beatUpload: "goats.mp3",
-    })
-    .then(function (docRef) {
-      console.log("Document written ");
-    })
-    .catch(function (error) {
-      console.log("Error adding document: ", error);
-    });
+  const onSubmit = () => {
+    db.collection("Beats")
+      .add({
+        name: beatName,
+        beatUpload: beatUpload,
+      })
+      .then(function (docRef) {
+        console.log("Document written ");
+      })
+      .catch(function (error) {
+        console.log("Error adding document: ", error);
+      });
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="beatName"> Name: </label>
       <input name="beatName" id="beat-name" ref={register} />
       <br />
       <label htmlFor="beatUpload"> Upload your beat: </label>
       <br />
       <input type="file" name="beatUpload" id="beat-upload" />
-      <button>Submit</button>
+      <input type="submit" defaultValue="Submit" ref={register} />
     </form>
   );
 }
 
 export default UploadBeat;
+
+// To do:
+// - Add functionality for user to submit data in forms and it appears in Firestore
+// - How to get mp3 files uploaded to firestore
--- src/firebase.js
+++ src/firebase.js
@@ -17,5 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
 
-export const fb = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
+
+export default firebase;
